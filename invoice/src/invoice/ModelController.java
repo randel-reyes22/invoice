85983659374d85353fdba7c4884e6bf1d0704241
randel-reyes22 <randel.reyes@unc.edu.ph>
2019-11-16 13:28:55 +0800

    added the signature model -- running

--- invoice/src/invoice/ModelController.java
+++ invoice/src/invoice/ModelController.java
@@ -29,6 +29,9 @@ public class ModelController implements Initializable, compute {
     @FXML private TextField po_date;
     @FXML private TextField item_count;
     @FXML private TextField total_render_amount;
+    @FXML private TextField customer_address;
+    @FXML private TextField customer_name;
+    @FXML private TextField customer_signature;
     
     //button
     @FXML private Button btn_Add;
@@ -55,6 +58,11 @@ public class ModelController implements Initializable, compute {
     @FXML
     public void AddItem(ActionEvent event)
     {  
+        //customer class
+        c.setCustomer_name(customer_name.getText()); // set the customer name
+        c.setCustomer_address(customer_address.getText()); // set the customer address
+        
+        //item class
         i.setItem_name(description.getText()); //item name
         i.setQty(Integer.valueOf(qty.getText())); // quantity
         i.setPrice(Double.valueOf(price.getText())); // price
@@ -66,8 +74,11 @@ public class ModelController implements Initializable, compute {
         qty.clear();
         price.clear();
         
-        //sets the purchase order date
+        //sets the content of the textfields
         po_date.setText(d.getDate());
+        item_count.setText(String.valueOf(count_item(i.getQty())));
+        total_render_amount.setText(String.valueOf(computeTotalAmount(i.getMainTotalAmount())));
+        customer_signature.setText(c.getCustomer_name());
         
         //append to the text area
         area.appendText(i.getItem_name());
@@ -77,13 +88,11 @@ public class ModelController implements Initializable, compute {
         area.appendText(String.valueOf(i.getPrice()));
         area.appendText("             ");
         area.appendText(String.valueOf(i.getTotal_amount()));
-        area.appendText("\n");
-        
-        
-        item_count.setText(String.valueOf(count_item(i.getQty())));
-        total_render_amount.setText(String.valueOf(computeTotalAmount(i.getMainTotalAmount())));
+        area.appendText("\n");  
+
     }
        
+    //close the scene
     @FXML
     public void closeWindow(ActionEvent event)
     {
@@ -91,18 +100,19 @@ public class ModelController implements Initializable, compute {
         stage.close();
     }
 
+    //computes the item price
     @Override
     public double computePrice(double price1, int qty1) {
-        
         return price1 * qty1;
-        
     }
-
+    
+    //compute the total amount of all the items
     @Override
     public double computeTotalAmount(double mainTotalAmount) {
         return TotalAmount = TotalAmount + mainTotalAmount;
     }
 
+    //count the quantity of the each items
     @Override
     public int count_item(int item) {
         ctr = ctr +  item;
